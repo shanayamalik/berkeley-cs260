2cc2d48ea47c6636df797d20c535eb4eb38c98d1
Shanaya Malik <139424427+shanayamalik@users.noreply.github.com>
2025-09-09 14:54:12 -0700

    Add files via upload

---
+++ code/part3/solution.js
@@ -0,0 +1,39 @@
+// Example: author tags
+const task0_cssSelector = "";
+const task0_jsCode = () => {
+  const selectedElements = [];
+
+  return selectedElements;
+};
+
+// Intro section
+const task1_cssSelector = "";
+const task1_jsCode = () => {
+  const selectedElements = [];
+
+  return selectedElements;
+};
+
+// Intro header
+const task2_cssSelector = "";
+const task2_jsCode = () => {
+  const selectedElements = [];
+
+  return selectedElements;
+};
+
+// Active buttons
+const task3_cssSelector = "";
+const task3_jsCode = () => {
+  const selectedElements = [];
+
+  return selectedElements;
+};
+
+// Post titles
+const task4_cssSelector = "";
+const task4_jsCode = () => {
+  const selectedElements = [];
+
+  return selectedElements;
+};
