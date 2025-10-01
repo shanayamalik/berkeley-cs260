0be9956abe18fc2f50efbb29e7f210ae5daaf063
shanayamalik <shanaya.malik@gmail.com>
2025-10-01 16:37:29 -0700

    navigating between pages does not lose information about which items have been added to bags

--- programming-assignment-3/code/src/App.jsx
+++ programming-assignment-3/code/src/App.jsx
@@ -6,14 +6,78 @@ import Summary from "./pages/summary/Summary";
 
 function App() {
   const [page, setPage] = useState("luggage-overview");
+  
+  // Luggage state - lifted up from Planner
+  const [personalItemItems, setPersonalItemItems] = useState([]);
+  const [carryOnItems, setCarryOnItems] = useState([]);
+  const [checkedBagItems, setCheckedBagItems] = useState([]);
+  const [checkedBag2Items, setCheckedBag2Items] = useState([]);
+  
+  // Cabin class selection (default to basic-economy)
+  const [cabinClass, setCabinClass] = useState("basic-economy");
+  
+  // Weight unit preference (default to lbs)
+  const [weightUnit, setWeightUnit] = useState("lbs");
+  
+  // Form state for adding new items to personal item bag
+  const [personalItemName, setPersonalItemName] = useState("");
+  const [personalItemWeight, setPersonalItemWeight] = useState("");
+  
+  // Form state for adding new items to carry-on bag
+  const [carryOnName, setCarryOnName] = useState("");
+  const [carryOnWeight, setCarryOnWeight] = useState("");
+  
+  // Form state for adding new items to checked bag
+  const [checkedBagName, setCheckedBagName] = useState("");
+  const [checkedBagWeight, setCheckedBagWeight] = useState("");
+  
+  // Form state for adding new items to checked bag 2
+  const [checkedBag2Name, setCheckedBag2Name] = useState("");
+  const [checkedBag2Weight, setCheckedBag2Weight] = useState("");
+
+  // Create luggage data object to pass to child components
+  const luggageData = {
+    personalItemItems,
+    carryOnItems,
+    checkedBagItems,
+    checkedBag2Items,
+    cabinClass,
+    weightUnit,
+    personalItemName,
+    personalItemWeight,
+    carryOnName,
+    carryOnWeight,
+    checkedBagName,
+    checkedBagWeight,
+    checkedBag2Name,
+    checkedBag2Weight
+  };
+
+  // Create luggage actions object to pass to Planner
+  const luggageActions = {
+    setPersonalItemItems,
+    setCarryOnItems,
+    setCheckedBagItems,
+    setCheckedBag2Items,
+    setCabinClass,
+    setWeightUnit,
+    setPersonalItemName,
+    setPersonalItemWeight,
+    setCarryOnName,
+    setCarryOnWeight,
+    setCheckedBagName,
+    setCheckedBagWeight,
+    setCheckedBag2Name,
+    setCheckedBag2Weight
+  };
 
   return (
     <div className="app">
       <Sidebar setPage={setPage} />
       <div className="main">
         {page === "luggage-overview" ? <LuggageOverview /> : null}
-        {page === "planner" ? <Planner /> : null}
-        {page === "summary" ? <Summary /> : null}
+        {page === "planner" ? <Planner luggageData={luggageData} luggageActions={luggageActions} /> : null}
+        {page === "summary" ? <Summary luggageData={luggageData} /> : null}
       </div>
     </div>
   );
--- programming-assignment-3/code/src/pages/planner/Planner.jsx
+++ programming-assignment-3/code/src/pages/planner/Planner.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { LUGGAGE_TYPES } from "../../data/luggageTypes";
 
 // Reusable component for individual items
@@ -20,33 +19,42 @@ function Item({ id, name, weight, unit, onDelete }) {
   );
 }
 
-export default function Planner() {
-  const [personalItemItems, setPersonalItemItems] = useState([]);
-  const [carryOnItems, setCarryOnItems] = useState([]);
-  const [checkedBagItems, setCheckedBagItems] = useState([]);
-  const [checkedBag2Items, setCheckedBag2Items] = useState([]);
-  
-  // Cabin class selection (default to basic-economy)
-  const [cabinClass, setCabinClass] = useState("basic-economy");
-  
-  // Weight unit preference (default to lbs)
-  const [weightUnit, setWeightUnit] = useState("lbs");
-  
-  // Form state for adding new items to personal item bag
-  const [personalItemName, setPersonalItemName] = useState("");
-  const [personalItemWeight, setPersonalItemWeight] = useState("");
-  
-  // Form state for adding new items to carry-on bag
-  const [carryOnName, setCarryOnName] = useState("");
-  const [carryOnWeight, setCarryOnWeight] = useState("");
-  
-  // Form state for adding new items to checked bag
-  const [checkedBagName, setCheckedBagName] = useState("");
-  const [checkedBagWeight, setCheckedBagWeight] = useState("");
-  
-  // Form state for adding new items to checked bag 2
-  const [checkedBag2Name, setCheckedBag2Name] = useState("");
-  const [checkedBag2Weight, setCheckedBag2Weight] = useState("");
+export default function Planner({ luggageData, luggageActions }) {
+  // Destructure luggage data from props
+  const {
+    personalItemItems,
+    carryOnItems,
+    checkedBagItems,
+    checkedBag2Items,
+    cabinClass,
+    weightUnit,
+    personalItemName,
+    personalItemWeight,
+    carryOnName,
+    carryOnWeight,
+    checkedBagName,
+    checkedBagWeight,
+    checkedBag2Name,
+    checkedBag2Weight
+  } = luggageData;
+
+  // Destructure luggage actions from props
+  const {
+    setPersonalItemItems,
+    setCarryOnItems,
+    setCheckedBagItems,
+    setCheckedBag2Items,
+    setCabinClass,
+    setWeightUnit,
+    setPersonalItemName,
+    setPersonalItemWeight,
+    setCarryOnName,
+    setCarryOnWeight,
+    setCheckedBagName,
+    setCheckedBagWeight,
+    setCheckedBag2Name,
+    setCheckedBag2Weight
+  } = luggageActions;
 
   // Get luggage type data
   const personalItemData = LUGGAGE_TYPES.find(type => type.name === "Personal Item");
