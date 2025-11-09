853171cf725b0c83a2d40b7f6fbd291340e0ee2a
shanayamalik <shanaya.malik@gmail.com>
2025-11-09 13:46:50 -0800

    moved final project to other directory

--- final/src/components/ui.js
+++
@@ -1,3 +0,0 @@
-// Reusable UI Components
-
-// TODO: Create reusable UI elements (buttons, cards, forms, etc.)
--- final/src/utils/helpers.js
+++
@@ -1,3 +0,0 @@
-// Utility Functions and Helpers
-
-// TODO: Add helper functions (formatting, validation, storage, etc.)
